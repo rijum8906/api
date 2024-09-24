49426d4a41ab5b0e5d460807130e7625835a318a
rijum8906 <83529834+rijum8906@users.noreply.github.com>
2024-09-24 20:05:43 +0530

    update routes/apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const loginController = require("./../controllers/loginController")
-const registrationController = require("./controllers/registrationController")
+const registrationController = require("./controllers/registrationController");
+const messageController = require("./controllers/messageController");
 
 const Router = express.Router();
 
 Router.post("/login-user",loginController);
 Router.put("/register-user",registrationController);
-Router.post("/message")
+Router.post("/send-message",messageController);
