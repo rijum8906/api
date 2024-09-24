48894e3d6d003d50ee09737d78ac9d81786b4449
rijum8906 <83529834+rijum8906@users.noreply.github.com>
2024-09-24 21:02:42 +0530

    update routes/apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,12 @@
-const express = require("express");
-const loginController = require("./../controllers/loginController")
-const registrationController = require("./controllers/registrationController");
-const messageController = require("./controllers/messageController");
+const express = require('express');
+const loginController = require('./../controllers/loginController');
+const registrationController = require('./../controllers/registrationController');
+const messageController = require('./../controllers/messageController');
+const verifyToken = require('./../miidlewares/verifyToken');
 
 const Router = express.Router();
 
-Router.post("/login-user",loginController);
-Router.put("/register-user",registrationController);
-Router.post("/send-message",messageController);
+Router.post('/login-user', loginController);
+Router.put('/register-user', registrationController);
+Router.post('/send-message', verifyToken, messageController.sendMessage);
+Router.get("/receive-message",verifyToken,messageController.receiveMessage)
