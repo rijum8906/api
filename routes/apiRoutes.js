74dc539b436f7ba0ff36d0bcdb1737c5f099735b
rijum8906 <83529834+rijum8906@users.noreply.github.com>
2024-09-26 21:00:12 +0530

    Add files via upload

---
+++ routes/apiRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const loginController = require('./../controllers/loginController');
+const registrationController = require('./../controllers/registrationController');
+const messageController = require('./../controllers/messageController');
+const verifyToken = require('./../miidlewares/verifyToken');
+
+const Router = express.Router();
+
+Router.post('/login-user', loginController);
+Router.put('/register-user',   registrationController);
+Router.post('/send-message', verifyToken, messageController.sendMessage);
+Router.post('/get-messages', verifyToken, messageController.receiveMessage);
+
+module.exports = Router;
