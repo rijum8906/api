f989df4aa1c9c7f026844f6299fdfc0cba79eff1
rijum8906 <83529834+rijum8906@users.noreply.github.com>
2024-09-26 20:59:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const fs = require("fs");
+const dbConn = require("./config/db");
+const apiRoutes = require("./routes/apiRoutes");
+
+
+dbConn();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+// api routes
+app.use("/api",apiRoutes);
+
+app.get("/",(req,res,next)=>{
+   res.send("Hello World")
+});
+
+app.listen(8000, () => {
+    console.log("app is running at port 80");
+});
